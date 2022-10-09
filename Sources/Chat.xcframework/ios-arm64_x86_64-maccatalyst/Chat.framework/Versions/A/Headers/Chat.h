0f428dc1b8860aef039d30bb915c7d1b2cd68eb5
mgabouarab <mgabouarab@gmail.com>
2022-10-10 00:49:55 +0200

    add chat framework

---
+++ Sources/Chat.xcframework/ios-arm64_x86_64-maccatalyst/Chat.framework/Versions/A/Headers/Chat.h
@@ -0,0 +1,18 @@
+//
+//  Chat.h
+//  Chat
+//
+//  Created by MGAbouarab on 10/09/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Chat.
+FOUNDATION_EXPORT double ChatVersionNumber;
+
+//! Project version string for Chat.
+FOUNDATION_EXPORT const unsigned char ChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chat/PublicHeader.h>
+
+
